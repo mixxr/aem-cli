1ff43f2f27b0b94cb444aed16d9467e29dd138e1
Michelangelo <serpico@adobe.com>
2018-12-06 00:53:18 +0100

    info, ping,purge,users

---
+++ farm-exec.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+printf "=======\nAEM CLI\n=======\nreading farm configuration from $1\n"
+CMD=cmds/"$2"
+
+# if CMD does not exists
+if [ ! -e "$CMD" ]; then
+    echo $2" command not found!"
+    exit 1
+fi
+
+while read -r host user 
+do 
+    
+    if [[ $host == "--" && $user == "END" ]]; then 
+        echo "<--"
+        exit 0
+    fi
+    if [[ $host == "--" && $user == "START" ]]; then 
+        START=1
+        echo "-->"
+        continue
+    fi
+
+    if [[ $START == 1 && ! -z "$host" && ! -z "$user" ]]; then
+        var=$($CMD $host $user ${@:3})
+        RC=$?
+        if [ $RC -ne 0 ]; then
+            printf "<--\n$2 exits with error: $var\n"
+            exit $RC
+        else
+            echo $host $user $var
+        fi
+    fi
+done < $1
